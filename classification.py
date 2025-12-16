ef3072ffc3f9bbea9ddc234e681922dfde03dd14
Nakki Vene <no>
2025-12-16 15:34:24 +0100

    fix confusing title

--- classification.py
+++ classification.py
@@ -55,7 +55,7 @@ class MelanomaClassifier:
         3: {'name': 'DF', 'full': 'Dermatofibroma', 'type': 'Benign'},
         4: {'name': 'MEL', 'full': 'Melanoma', 'type': 'Malignant'},
         5: {'name': 'NV', 'full': 'Melanocytic nevus', 'type': 'Benign'},
-        6: {'name': 'VASC', 'full': 'Vascular lesion', 'type': 'Benign/Malignant'}
+        6: {'name': 'VASC', 'full': 'Vascular lesion', 'type': 'Indecidable'}
     }
     
     def __init__(self, models_dir: str = 'models'):
